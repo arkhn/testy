5051fe614a5b05c53644a590c30ba3a91ea0252f
Jasopaum <jason.paumier@gmail.com>
2021-02-05 13:49:34 +0100

    Update river api url

--- tests/settings.py
+++ tests/settings.py
@@ -16,7 +16,7 @@ PYROG_API_URL = f"http://{PYROG_API_HOST}:{PYROG_API_PORT}/"
 
 RIVER_API_HOST = os.environ.get("RIVER_API_HOST", "river-api")
 RIVER_API_PORT = int(os.environ.get("RIVER_API_PORT", "3000"))
-RIVER_API_URL = f"http://{RIVER_API_HOST}:{RIVER_API_PORT}"
+RIVER_API_URL = f"http://{RIVER_API_HOST}:{RIVER_API_PORT}/api"
 
 KAFKA_HOST = os.environ.get("KAFKA_HOST", "kafka")
 KAFKA_PORT = os.environ.get("KAFKA_PORT", "9092")
